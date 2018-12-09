59a8b2c361afbf615f58b4fb352e4251e706e196
tropicbrush <42104722+tropicbrush@users.noreply.github.com>
2018-12-08 19:41:32 -0500

    Create grantAuthScope.js

---
+++ Auth0 Conf/grantAuthScope.js
@@ -0,0 +1,33 @@
+function (user, context, callback) {
+  // TODO: implement your rule
+if (context.clientID === 'lnRKX3VDp6qZwRbTCsmR1p5oMkzSw5ke')
+  {
+  
+  if (user.app_metadata.title === 'EndUser') {
+    
+   context.accessToken.scope = ['TimeOffTool.me.read', 'TimeOffTool.me.write'];
+  }
+  else if (user.app_metadata.title === 'Manager')
+  {
+   context.accessToken.scope = ['TimeOffTool.me.read', 'TimeOffTool.me.write','TimeOffTool.DR.read', 'TimeOffTool.DR.approve'];
+  //  currentScope.concat(['TimeOfTool.DR.read', 'TimeOfTool.DR.Approve']);
+  }
+  else if (user.app_metadata.title === 'HR Admin')
+  {
+      context.accessToken.scope = ['TimeOffTool.me.read', 'TimeOffTool.me.write','TimeOffTool.HR.admin'];
+
+  // currentScope.concat(['TimeOffTool.HR.admin']);
+  }
+  else
+  {
+  return callback(new Error("Access Denied : User Title not available or invalid title."));
+  
+  }
+
+  callback(null, user, context);
+  }
+  else 
+  {
+  callback(null, user, context);
+  }
+}
